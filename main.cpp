7b6df6f2758ed4f28281c16a5695d06bab302060
imaxii <maxigroxx@gmail.com>
2026-04-12 16:00:38 -0300

    modifico main.cpp

--- main.cpp
+++ main.cpp
@@ -16,43 +16,38 @@ void error(const char *msg){
 }
 
 int main(){
-    int sockfd, newsockfd;
-    socklen_t clilen;
+    int serverSocket;
     struct sockaddr_in serv_addr, cli_addr;
     int n;
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket < 0)
         error("ERROR opening socket");
     bzero((char *)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(3000);
-    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    if (bind(serverSocket, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR on binding");
-    listen(sockfd, 5);
-    clilen = sizeof(cli_addr);
-    newsockfd = accept(sockfd,(struct sockaddr *)&cli_addr,&clilen);
-    if (newsockfd < 0)
+    listen(serverSocket, 5);
+    socklen_t clilen = sizeof(cli_addr);
+    int clientSocket = accept(serverSocket,(struct sockaddr *)&cli_addr,&clilen);
+    if (clientSocket < 0)
         error("ERROR on accept");
-   // bzero(buffer, 256);
-    // n = read(newsockfd, buffer, 255);
-    // if (n < 0)
-    //     error("ERROR reading from socket");
-    // printf("Here is the message: %s\n", buffer);
     if (FILE *fp = fopen("filename.txt", "wb")){
-    size_t readBytes;
-    char buffer[4096];
-        while ((readBytes = recv(newsockfd, buffer, sizeof(buffer), 0)) > 0){
+        size_t readBytes;
+        char buffer[4096];
+        while ((readBytes = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0){
             if (fwrite(buffer, 1, readBytes, fp) != readBytes){
                 cout<<"error leyendo";
                 break;
             }
+            std::cout << "Received " << readBytes << " bytes" << std::endl;
         }
+        std::cout << "File transfer complete." << std::endl;
+        fclose(fp);
     }
-    // n = write(newsockfd, "I got your message", 18);
-    // if (n < 0)
-    //     error("ERROR writing to socket");
-    close(newsockfd);
-    close(sockfd);
+  
+    close(clientSocket);
+    close(serverSocket);
     return 0;
 }
