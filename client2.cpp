689879d1f41ff525e8f06cc0ddc0e9c8b4103764
= <maxigroxx@gmail.com>
2026-04-13 21:34:19 -0300

    agrego cambios en el main.cpp y agrego nuevo client.cpp

---
+++ client2.cpp
@@ -0,0 +1,30 @@
+#include <cstring>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main()
+{
+    // creating socket
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // specifying address
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(3000);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    // sending connection request
+    connect(clientSocket, (struct sockaddr*)&serverAddress,
+            sizeof(serverAddress));
+
+    // sending data
+    long message = 134345345;
+    send(clientSocket, &message, sizeof(message), 0);
+
+    // closing socket
+    close(clientSocket);
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -7,18 +7,14 @@
 #include <netinet/in.h>
 #include <iostream>
 
-using namespace std;
-
 void error(const char *msg){
     perror(msg);
-    cout<<msg;
     exit(1);
 }
 
 int main(){
     int serverSocket;
     struct sockaddr_in serv_addr, cli_addr;
-    int n;
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket < 0)
         error("ERROR opening socket");
@@ -33,20 +29,24 @@ int main(){
     int clientSocket = accept(serverSocket,(struct sockaddr *)&cli_addr,&clilen);
     if (clientSocket < 0)
         error("ERROR on accept");
-    if (FILE *fp = fopen("filename.txt", "wb")){
+    long fileSize;
+    if(recv(clientSocket, &fileSize, sizeof(long), 0)<0)
+        error("cannot read file size");
+    std::cout<<fileSize<<std::endl;
+    if (FILE *fp = fopen("image.jpeg", "wb")){
         size_t readBytes;
         char buffer[4096];
-        while ((readBytes = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0){
+        while ((readBytes = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0 && fileSize>0){
             if (fwrite(buffer, 1, readBytes, fp) != readBytes){
-                cout<<"error leyendo";
+                std::cout<<"error leyendo";
                 break;
             }
+            fileSize-=readBytes;
             std::cout << "Received " << readBytes << " bytes" << std::endl;
         }
         std::cout << "File transfer complete." << std::endl;
         fclose(fp);
     }
-  
     close(clientSocket);
     close(serverSocket);
     return 0;
