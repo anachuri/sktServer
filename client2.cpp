2133a1b576a731d03625b9ba20b1d1ee6cc048e6
= <maxigroxx@gmail.com>
2026-04-15 11:01:42 -0300

    agrego cmakeList para poder compilar usando qtcreator

--- client2.cpp
+++ client2.cpp
@@ -4,27 +4,22 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-int main()
-{
+int main() {
+    std::cout<<"client"<<std::endl;
+    std::cout<<"client"<<std::endl;
     // creating socket
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-
     // specifying address
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(3000);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
-
     // sending connection request
-    connect(clientSocket, (struct sockaddr*)&serverAddress,
-            sizeof(serverAddress));
-
+    connect(clientSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
     // sending data
     long message = 134345345;
     send(clientSocket, &message, sizeof(message), 0);
-
     // closing socket
     close(clientSocket);
-
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,47 +1,49 @@
+#include <iostream>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
 
-void error(const char *msg){
+void error(const char *msg) {
     perror(msg);
     exit(1);
 }
 
-int main(){
+int main() {
+    std::cout<<"server"<<std::endl;
     int serverSocket;
     struct sockaddr_in serv_addr, cli_addr;
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket < 0)
         error("ERROR opening socket");
-    bzero((char *)&serv_addr, sizeof(serv_addr));
+    bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(3000);
-    if (bind(serverSocket, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    if (bind(serverSocket, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR on binding");
     listen(serverSocket, 5);
     socklen_t clilen = sizeof(cli_addr);
-    int clientSocket = accept(serverSocket,(struct sockaddr *)&cli_addr,&clilen);
+    int clientSocket = accept(serverSocket, (struct sockaddr *) &cli_addr, &clilen);
     if (clientSocket < 0)
         error("ERROR on accept");
     long fileSize;
-    if(recv(clientSocket, &fileSize, sizeof(long), 0)<0)
+    if (recv(clientSocket, &fileSize, sizeof(long), 0) < 0)
         error("cannot read file size");
-    std::cout<<fileSize<<std::endl;
-    if (FILE *fp = fopen("image.jpeg", "wb")){
+    std::cout << fileSize << std::endl;
+    if (FILE *fp = fopen("image.jpeg", "wb")) {
         size_t readBytes;
         char buffer[4096];
-        while ((readBytes = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0 && fileSize>0){
-            if (fwrite(buffer, 1, readBytes, fp) != readBytes){
-                std::cout<<"error leyendo";
+        std::cout << "entrando" << std::endl;
+        while ((readBytes = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
+            if (fwrite(buffer, 1, readBytes, fp) != readBytes) {
+                std::cout << "error leyendo";
                 break;
             }
-            fileSize-=readBytes;
+            fileSize -= readBytes;
             std::cout << "Received " << readBytes << " bytes" << std::endl;
         }
         std::cout << "File transfer complete." << std::endl;
