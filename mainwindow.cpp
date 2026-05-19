b95e792898bcfd1faf5db122ade6668f64bcee38
imaxii <maxigroxx@gmail.com>
2026-05-19 01:35:19 -0300

    agrego cambios en la interfaz y agrego nuevos actions para manejar tanto el fileExplorer como los archivos recibidos

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+#include <QFileSystemModel>
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
@@ -6,8 +7,34 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow) {
     ui->setupUi(this);
     ui->toolBar->setContextMenuPolicy(Qt::PreventContextMenu);
+
+    QString sPath = "/home";
+    dirModel = new QFileSystemModel(this);
+    dirModel->setFilter(QDir::NoDotAndDotDot | QDir::AllDirs);
+    dirModel->setRootPath(sPath);
+
+    ui->treeView->setModel(dirModel);
+    ui->treeView->hideColumn(1);
+    ui->treeView->hideColumn(2);
+    ui->treeView->setColumnWidth(0, 200);
+
+    QModelIndex index = dirModel->index(sPath, 0); // this line is not in the video.
+    ui->treeView->setRootIndex(index);             // this line is not in the video.
+
+    fileModel = new QFileSystemModel(this);
+    fileModel->setFilter(QDir::NoDotAndDotDot | QDir::Files);
+    dirModel->setRootPath(sPath);
+
+    ui->listView->setModel(fileModel);
+    index = fileModel->index(sPath, 0); // this line is not in the video.
+    ui->listView->setRootIndex(index);  // this line is not in the video.
 }
 
 MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::on_treeView_clicked(const QModelIndex &index) {
+    QString path = dirModel->fileInfo(index).absoluteFilePath();
+    ui->listView->setRootIndex(fileModel->setRootPath(path));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+class QFileSystemModel;
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -16,7 +17,12 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_treeView_clicked(const QModelIndex &index);
+
 private:
     Ui::MainWindow *ui;
+    QFileSystemModel *dirModel;
+    QFileSystemModel *fileModel;
 };
 #endif // MAINWINDOW_H
